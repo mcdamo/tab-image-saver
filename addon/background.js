d868af858065d2087907eaa4e5f0fd8d832a7fa6
mcdamo <github@meku.org>
2017-08-11 12:56:28 +1000

    Freshly linted code

--- addon/background.js
+++ addon/background.js
@@ -1,266 +1,275 @@
-
-// these are defined in options.js
-var ACTION;
-var ACTIVE_TAB;
-var CLOSE_TAB;
-var DOWNLOAD_PATH; // Empty to use Downloads directory or specify subfolder within Downloads directory (no leading or trailing slashes)
-var CONFLICT_ACTION;
-var MIN_WIDTH;
-var MIN_HEIGHT;
-var NOTIFY_ENDED;
-var REMOVE_ENDED;
-// these are used at runtime
-var SCRIPT_MAXIMAGE = '/content-maximage.js'; // content script to find largest image in tab
-var SCRIPT_TABIMAGE = '/content-tabimage.js'; // content script to test if tab is a direct image
-var CONTENT_SCRIPT = SCRIPT_MAXIMAGE;
-var TABS_LOADED = 0; // number of tabs executed
-var TABS_ENDED = 0; // number of tabs returned a message
-var IMAGES_SAVED = 0; // valid images saved
-var IMAGES_SKIPPED = 0; // invalid images
-const DEBUG_LOG = false;
-
-function init() {
-	TABS_LOADED = 0;
-	TABS_ENDED = 0;
-	IMAGES_SAVED = 0;
-	IMAGES_SKIPPED = 0;
-	getOptions(executeTabs);
-}
+/* Global variables */
+let ACTION;
+let ACTIVE_TAB;
+let CLOSE_TAB;
+let DOWNLOAD_PATH; // Empty to use Downloads directory or specify subfolder within Downloads directory (no leading or trailing slashes)
+let CONFLICT_ACTION;
+let MIN_WIDTH;
+let MIN_HEIGHT;
+let NOTIFY_ENDED;
+let REMOVE_ENDED;
+let SCRIPT_MAXIMAGE = "/content-maximage.js"; // content script to find largest image in tab
+let SCRIPT_TABIMAGE = "/content-tabimage.js"; // content script to test if tab is a direct image
+let CONTENT_SCRIPT = SCRIPT_MAXIMAGE;
+let TABS_LOADED = 0; // number of tabs executed
+let TABS_ENDED = 0; // number of tabs returned a message
+let IMAGES_SAVED = 0; // valid images saved
+let IMAGES_SKIPPED = 0; // invalid images
 
 function debug(message) {
-	if (DEBUG_LOG) {
-		console.log(message);
-	}
+  console.log(message);
 }
 
 function getOptions(callback) {
-	var getting = browser.storage.local.get();
-	getting.then(result => {
-		debug("Options: ");
-		for (let o in result) {
-			debug(`${o}: ${result[o]}`);
-		}
-		ACTION = result.action || "current";
-		ACTIVE_TAB = result.activeTab;
-		CLOSE_TAB = result.closeTab;
-		DOWNLOAD_PATH = result.downloadPath || "";
-		if (DOWNLOAD_PATH.length > 0) {
-			DOWNLOAD_PATH += "/";
-		}
-		CONFLICT_ACTION = result.conflictAction || "uniquify";
-		MIN_WIDTH = result.minWidth || "100";
-		MIN_HEIGHT = result.minHeight || "100";
-		if (null == result.notifyEnded) {
-			result.notifyEnded = true;
-		}
-		NOTIFY_ENDED = result.notifyEnded;
-		REMOVE_ENDED = result.removeEnded;
-		if (result.tabImage) {
-			CONTENT_SCRIPT = SCRIPT_TABIMAGE;
-		}
-		callback();
-	}, error => {
-/*
-		notify("error", {
-			"title": "Error",
-			"content": "
-*/
-		debug("OptionsError: "+error);
-	});
-}
-
-function executeTabs() {
-	//decide which action to take
-	debug("Action: "+ACTION);
-	switch(ACTION) {
-	case "all":
-		tabsAll();
-		break;
-	case "left":
-		tabsLeft();
-		break;
-	case "right":
-		tabsRight();
-		break;
-	case "current":
-	default:
-		tabsCurrent();
-		break;
-	}
+  let getting = browser.storage.local.get();
+  getting.then(result => {
+    debug("Options: ");
+    for (const [key, value] of result) {
+      debug(`${key}: ${value}`);
+    }
+    ACTION = result.action || "current";
+    ACTIVE_TAB = result.activeTab;
+    CLOSE_TAB = result.closeTab;
+    DOWNLOAD_PATH = result.downloadPath || "";
+    if (DOWNLOAD_PATH.length > 0) {
+      DOWNLOAD_PATH += "/";
+    }
+    CONFLICT_ACTION = result.conflictAction || "uniquify";
+    MIN_WIDTH = result.minWidth || "100";
+    MIN_HEIGHT = result.minHeight || "100";
+    if (result.notifyEnded === null) {
+      result.notifyEnded = true;
+    }
+    NOTIFY_ENDED = result.notifyEnded;
+    REMOVE_ENDED = result.removeEnded;
+    if (result.tabImage) {
+      CONTENT_SCRIPT = SCRIPT_TABIMAGE;
+    }
+    callback();
+    return;
+  }).catch(error => {
+    // notify("error", {
+    //      "title": "Error",
+    //      "content": "
+    debug(`OptionsError: ${error}`);
+    // console.warn, console.error
+    return false;
+  });
 }
 
 function executeTab(tab) {
-	TABS_LOADED++;
-	let tabid = tab.id;
-	debug(`Sending tab ${tabid} (LOADED ${TABS_LOADED}): ${CONTENT_SCRIPT}`);
-	var executing = browser.tabs.executeScript(
-		tabid, {
-		file: CONTENT_SCRIPT
-	});
+  TABS_LOADED++;
+  let tabid = tab.id;
+  debug(`Sending tab ${tabid} (LOADED ${TABS_LOADED}): ${CONTENT_SCRIPT}`);
+  let executing = browser.tabs.executeScript(tabid, {file: CONTENT_SCRIPT});
+  executing.then(result => {
+    // TODO: returned value from executed script
+    debug(`executeTab: ${result}`);
+    return result;
+  }).catch(error => {
+    // TODO
+  });
+}
 
+function getCurrentWindowTabs() {
+  return browser.tabs.query({currentWindow: true});
+}
+
+function callOnActiveTab(callback) {
+  getCurrentWindowTabs().then(tabs => {
+    for (let tab of tabs) {
+      if (tab.active) {
+        callback(tab, tabs);
+      }
+    }
+    return;
+  }).catch(error => {
+    // TODO
+  });
 }
 
 /* execute all tabs */
 function tabsAll() {
-	getCurrentWindowTabs().then((tabs) => {
-		for (var tab of tabs) {
-			if (tab.active && ACTIVE_TAB == false) {
-				continue;
-			}
-			executeTab(tab);
-		}
-	});
+  getCurrentWindowTabs().then(tabs => {
+    for (let tab of tabs) {
+      if (tab.active && !ACTIVE_TAB) {
+        continue;
+      }
+      executeTab(tab);
+    }
+    return;
+  }).catch(error => {
+    // TODO
+  });
 }
 
 /* execute on active tab */
 function tabsCurrent() {
-	callOnActiveTab((tab, tabs) => {
-		executeTab(tab);
-	});
+  callOnActiveTab((tab, tabs) => {
+    executeTab(tab);
+  });
 }
 
 /* execute tabs to the LEFT of the active tab */
 function tabsLeft() {
-	var returnNow = false;
-	getCurrentWindowTabs().then((tabs) => {
-		for (var tab of tabs) {
-			if (tab.active) {
-				returnNow = true;
-				if (false == ACTIVE_TAB) {
-					return;
-				}
-			}
-			executeTab(tab);
-			if (returnNow) {
-				return;
-			}
-		}
-	});
-};
+  let returnNow = false;
+  getCurrentWindowTabs().then(tabs => {
+    for (let tab of tabs) {
+      if (tab.active) {
+        returnNow = true;
+        if (!ACTIVE_TAB) {
+          return;
+        }
+      }
+      executeTab(tab);
+      if (returnNow) {
+        return;
+      }
+    }
+    return;
+  }).catch(error => {
+    // TODO
+  });
+}
 
 /* execute tabs to the right of the active tab */
 function tabsRight() {
-	callOnActiveTab((tab, tabs) => {
-		debug(`Starting at id: ${tab.id}, index: ${tab.index}`);
-		let next = tab.index;
-		if(ACTIVE_TAB == false) {
-			if((tab.index+1) == tabs.length) {
-				// already at last tab
-				debug("Nothing to do.");
-				return;
-			}
-			next = tab.index+1;
-		}
-		for (let i = next; i < tabs.length; i++) {
-			executeTab(tabs[i]);
-		}
-	});
+  callOnActiveTab((tab, tabs) => {
+    debug(`Starting at id: ${tab.id}, index: ${tab.index}`);
+    let next = tab.index;
+    if (!ACTIVE_TAB) {
+      if ((tab.index + 1) === tabs.length) {
+        // already at last tab
+        debug("Nothing to do.");
+        return;
+      }
+      next = tab.index + 1;
+    }
+    for (let i = next; i < tabs.length; i++) {
+      executeTab(tabs[i]);
+    }
+  });
 }
 
-function getCurrentWindowTabs() {
-	return browser.tabs.query({currentWindow: true});
+function executeTabs() {
+  debug(`Action: ${ACTION}`);
+  switch (ACTION) {
+    case "all":
+      tabsAll();
+      break;
+    case "left":
+      tabsLeft();
+      break;
+    case "right":
+      tabsRight();
+      break;
+    case "current":
+    default:
+      tabsCurrent();
+      break;
+  }
 }
 
-function callOnActiveTab(callback) {
-	getCurrentWindowTabs().then((tabs) => {
-		for (var tab of tabs) {
-			if (tab.active) {
-				callback(tab, tabs);
-			}
-		}
-	});
+function onDownloadStarted(dlid, tabid, path, callback) {
+  IMAGES_SAVED++;
+  debug(`Download(${IMAGES_SAVED}) ${path}`);
+  if (CLOSE_TAB) {
+    let removing = browser.tabs.remove(tabid);
+    removing.then(result => {
+      // removed
+      debug(`Tab removed ${tabid}`);
+      return;
+    }).catch(error => {
+      // TODO
+    });
+  }
+  if (REMOVE_ENDED) {
+    browser.downloads.erase({"id": dlid});
+  }
+  callback();
+}
+function onDownloadFailed(e, path, callback) {
+  IMAGES_SKIPPED++;
+  console.error(`Download failed (${path}): ${e}`);
+  callback();
 }
 
 /* receive image from content-script (Tab) */
 function downloadImage(image, tabid, callback) {
-	if (!image) {
-		debug(`No image found`);
-		IMAGES_SKIPPED++;
-		return false;
-	}
-	debug(`${image.width}x${image.height} ${image.src}`);
-	var path = DOWNLOAD_PATH;
-	var url = image.src;
-	if (image.width < MIN_WIDTH || image.height < MIN_HEIGHT) {
-		debug("Dimensions smaller than required, not saving");
-		IMAGES_SKIPPED++;
-		return false;
-	}
-	path += url.replace(/^.*[\\\/]/, ''); // append filename from url
-	var downloading = browser.downloads.download({
-		url: url,
-		filename: path,
-		//saveAs: false, // not required, min_ver FF52
-		conflictAction: CONFLICT_ACTION
-	});
-	downloading.then(
-		function(dlid) { onDownloadStarted(dlid, tabid, path, callback); },
-		function(e) { onDownloadFailed(e, path, callback); }
-	);
-	return true;
-}
-
-function onDownloadStarted(dlid, tabid, path, callback) {
-	IMAGES_SAVED++;
-	debug(`Download(${IMAGES_SAVED}) ${path}`);
-	if (CLOSE_TAB) {
-		var removing = browser.tabs.remove(tabid);
-		//removing.then(onRemoved, onError);
-	}
-	if (REMOVE_ENDED) {
-		browser.downloads.erase({
-			"id": dlid
-		});
-	}
-	callback();
-}
-function onDownloadFailed(e, path, callback) {
-	IMAGES_SKIPPED++;
-	console.log(`Download failed (${path}): ${e}`);
-	callback();
+  if (!image) {
+    debug("No image found");
+    IMAGES_SKIPPED++;
+    return false;
+  }
+  debug(`${image.width}x${image.height} ${image.src}`);
+  let path = DOWNLOAD_PATH;
+  let url = image.src;
+  if (image.width < MIN_WIDTH || image.height < MIN_HEIGHT) {
+    debug("Dimensions smaller than required, not saving");
+    IMAGES_SKIPPED++;
+    return false;
+  }
+  path += url.replace(/^.*[/\\]/, ""); // append filename from url
+  let downloading = browser.downloads.download({
+    url,
+    filename: path,
+    // saveAs: false, // not required, min_ver FF52
+    conflictAction: CONFLICT_ACTION
+  });
+  // downloading.then(
+  //  function(dlid) { onDownloadStarted(dlid, tabid, path, callback); },
+  //  function(e) { onDownloadFailed(e, path, callback); }
+  // );
+  downloading.then(dlid => onDownloadStarted(dlid, tabid, path, callback))
+    .catch(error => onDownloadFailed(error, path, callback));
+  return true;
 }
 
 function notify(id, message) {
-	var title = message.title;
-	var content = message.content;
-	return browser.notifications.create(id, {
-		"type": "basic",
-		"iconUrl": browser.extension.getURL("icons/down-48.png"),
-		"title": title,
-		"message": content
-	});
+  return browser.notifications.create(id, {
+    "type": "basic",
+    "iconUrl": browser.extension.getURL("icons/down-48.png"),
+    "title": message.title,
+    "message": message.content
+  });
 }
 
 function notifyFinished()
 {
-	if (true != NOTIFY_ENDED) {
-		return;
-	}
-	if (TABS_LOADED != TABS_ENDED) {
-		return;
-	}
-	if (TABS_LOADED != (IMAGES_SAVED + IMAGES_SKIPPED)) {
-		return;
-	}
-	notify('finished', {
-		title: "Tab Image Saver",
-		content: `${IMAGES_SAVED} downloaded`
-	});
-	//setTimeout(browser.notifications.clear('finished'), 4000);
+  if (!NOTIFY_ENDED) {
+    return;
+  }
+  if (TABS_LOADED !== TABS_ENDED) {
+    return;
+  }
+  if (TABS_LOADED !== (IMAGES_SAVED + IMAGES_SKIPPED)) {
+    return;
+  }
+  notify("finished", {
+    title: "Tab Image Saver",
+    content: `${IMAGES_SAVED} downloaded`
+  });
+  // setTimeout(browser.notifications.clear('finished'), 4000);
 }
 
 function handleMessage(request, sender, sendResponse) {
-	TABS_ENDED++;
-	debug(`Response from tab ${sender.tab.id} (ENDED ${TABS_ENDED})`);
-	if (downloadImage(request.image, sender.tab.id, notifyFinished)) {
-/*
-		sendResponse({
-			response: "Thanks for the image"
-		});
-*/
-	} else {
-		notifyFinished();
-	}
+  TABS_ENDED++;
+  debug(`Response from tab ${sender.tab.id} (ENDED ${TABS_ENDED})`);
+  if (downloadImage(request.image, sender.tab.id, notifyFinished)) {
+    //  sendResponse({
+    //    response: "Thanks for the image"
+    //  });
+  } else {
+    notifyFinished();
+  }
+}
+
+function init() {
+  TABS_LOADED = 0;
+  TABS_ENDED = 0;
+  IMAGES_SAVED = 0;
+  IMAGES_SKIPPED = 0;
+  getOptions(executeTabs);
 }
 
 browser.runtime.onMessage.addListener(handleMessage);
--- addon/content-tabimage.js
+++ addon/content-tabimage.js
@@ -1,45 +1,42 @@
 (function() {
-	/*
-	Check and set a global guard variable.
-	If this content script is injected into the same page again,
-	it will do nothing next time.
-	*/
-	if (window.hasRun) {
-		return;
-	}
-	window.hasRun = true;
+  /*
+  Check and set a global guard variable.
+  If this content script is injected into the same page again,
+  it will do nothing next time.
+  */
+  if (window.hasRun) {
+    return;
+  }
+  window.hasRun = true;
 
-	function getMaxImage() {
-		maxImage = null;
-		if (0 === document.contentType.indexOf("image")) {
-			maxImage = document.images[0];
-		}
-		if (maxImage)
-			return {
-			src: maxImage.src,
-			width: maxImage.width,
-			height: maxImage.height
-		}
-		else
-			return null;
-	}
+  function getMaxImage() {
+    let maxImage = null;
+    if (document.contentType.indexOf("image") === 0) {
+      maxImage = document.images[0];
+    }
+    if (maxImage) {
+      return {
+        src: maxImage.src,
+        width: maxImage.width,
+        height: maxImage.height
+      };
+    }
+    return null;
+  }
 
+  function handleResponse(message) {
+    // console.log(`Message from the background script:  ${message.response}`);
+  }
 
-	function handleResponse(message) {
-		//console.log(`Message from the background script:  ${message.response}`);
-	}
+  function handleError(error) {
+    console.error(`Error: ${error}`);
+  }
 
-	function handleError(error) {
-		console.log(`Error: ${error}`);
-	}
+  function notifyBackgroundPage(e) {
+    let sending = browser.runtime.sendMessage({image: e});
+    sending.then(handleResponse)
+      .catch(error => handleError(error));
+  }
 
-
-	function notifyBackgroundPage(e) {
-		var sending = browser.runtime.sendMessage({
-			image: e
-		});
-		sending.then(handleResponse, handleError);  
-	}
-
-	notifyBackgroundPage(getMaxImage());
+  notifyBackgroundPage(getMaxImage());
 })();
